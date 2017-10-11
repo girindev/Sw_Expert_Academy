271237432d9966fff9f6233f22758ff643c625d9
rnsidgm <rnsidgm@users.noreply.github.com>
2017-10-11 12:03:28 +0900

    미생물 완료

--- SW_Expert_Academy/src/p2382/Solution.java
+++ SW_Expert_Academy/src/p2382/Solution.java
@@ -6,9 +6,11 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Solution {
+	static int cnt = 0;
 	static int N, M, K;
 	static int map[][];
 	static ArrayList<Micro> micro;
@@ -30,10 +32,12 @@ public class Solution {
 		}
 	}
 
-	static int bfs() {
-		int result = 0;
-		// Quque<>
-		return result;
+	static void init() {
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				map[i][j] = 0;
+			}
+		}
 	}
 
 	public static void main(String[] args) throws IOException {
@@ -44,17 +48,10 @@ public class Solution {
 			st = new StringTokenizer(br.readLine());
 			N = Integer.parseInt(st.nextToken());
 			M = Integer.parseInt(st.nextToken());
-			K =  Integer.parseInt(st.nextToken());
+			K = Integer.parseInt(st.nextToken());
 			map = new int[N][N];
 			micro = new ArrayList<>();
 			q = new LinkedList<>();
-			// int result = 0;
-			for (int n1 = 0; n1 < N; n1++) {
-				map[0][n1] = -1;
-				map[n1][0] = -1;
-				map[N - 1][n1] = -1;
-				map[n1][N - 1] = -1;
-			}
 			for (int k = 0; k < K; k++) {
 				st = new StringTokenizer(br.readLine());
 				int x = Integer.parseInt(st.nextToken());
@@ -64,49 +61,70 @@ public class Solution {
 				micro.add(new Micro(x, y, cnt, dir));
 			}
 			for (int m = 0; m < M; m++) {
+				// 이동
+				init(); // map 초기화
 				for (int i = 0; i < micro.size(); i++) {
 					Micro mc = micro.get(i);
 					mc.x += dx[mc.des - 1];
 					mc.y += dy[mc.des - 1];
-					if (mc.x != 0 && mc.x != N - 1 && mc.y != 0 && mc.y != N - 1) {
-						map[mc.x][mc.y] = 1;
-					} else {
-						if (mc.x == 0 || mc.x == N - 1) {
-							mc.cnt /= 2;
-							mc.des = 5 - mc.des;
-						} else if (mc.y == 0 || mc.y == N - 1) {
-							mc.cnt /= 2;
-							mc.des = 3 - mc.des;
-						}
+					map[mc.x][mc.y] = 1;
+
+					if (mc.x == 0 || mc.x == N - 1) {
+						mc.cnt /= 2;
+						mc.des = 3 - mc.des;
+					} else if (mc.y == 0 || mc.y == N - 1) {
+						mc.cnt /= 2;
+						mc.des = 7 - mc.des;
 					}
 				}
+				// 계산
 				for (int i = 0; i < N; i++) {
 					for (int j = 0; j < N; j++) {
 						if (map[i][j] == 1) {
-							int index = 0;
 							int sum = 0;
 							int temp1 = 0;
-							for (int x = 0; x < micro.size(); x++) {
-								if (micro.get(x).x == i && micro.get(x).y == j) {
-									sum += micro.get(x).cnt;
-									if (temp1 < micro.get(x).cnt) {
-										temp1 = micro.get(x).cnt;
-										index = x;
-									} else {
-										micro.remove(micro.get(x));
-									}
+
+							for (Micro micro : micro) {
+								if (micro.x == i && micro.y == j) {
+									sum += micro.cnt;
 								}
 							}
-							micro.get(index).cnt = sum;
+
+							Stack<Micro> s = new Stack<>();
+
+							for (Micro mc : micro) {
+								if (mc.x == i && mc.y == j) {
+									s.add(mc);
+								}
+							}
+
+							if (s.size() > 1) {
+								Micro tempMc = null;
+								int stackSize = s.size();
+								for (int s_size = 0; s_size < stackSize; s_size++) {
+									Micro mc = s.pop();
+									if (temp1 < mc.cnt) {
+										if (tempMc != null) {
+											micro.remove(tempMc);
+										}
+										tempMc = mc;
+										temp1 = mc.cnt;
+									} else {
+										micro.remove(mc);
+									}
+								}
+								tempMc.cnt = sum;
+							}
 						}
 					}
 				}
 			}
-			int ans =0;
-			for (int i=0; i<micro.size(); i++){
+			int ans = 0;
+			for (int i = 0; i < micro.size(); i++) {
 				ans += micro.get(i).cnt;
 			}
-			System.out.println(ans);
+			cnt++;
+			System.out.println("#" + cnt+" " + ans);
 		}
 	}
 }
